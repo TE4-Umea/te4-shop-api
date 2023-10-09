3134f17359c5a29d4833bb808e0fead6ca58349f
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-10-09 14:58:25 +0200

    More api gets

--- app.js
+++ app.js
@@ -8,13 +8,63 @@ app.use(cors())
 
 app.get('/', async function (req, res) {
     const allItems = await prisma.item.findMany({})
-    const allCategories = await prisma.category.findMany({
+
+    res.json({ data: allItems })
+})
+
+app.get('/product/:id', async function (req, res) {
+    const oneItems = await prisma.item.findMany({
+        where: {
+            id: parseInt(req.params.id),
+        },
+    })
+
+    res.json({ data: oneItems })
+})
+
+app.get('/category/', async function (req, res) {
+    const categ = await prisma.category.findMany({
         include: {
             items: true,
         },
     })
+    res.json({ data: categ })
+})
 
-    res.json({ data: allItems, categories: allCategories })
+app.get('/category/dog', async function (req, res) {
+    const dogCateg = await prisma.category.findMany({
+        where: {
+            name: 'Hundprodukter',
+        },
+        include: {
+            items: true,
+        },
+    })
+    res.json({ data: dogCateg })
+})
+
+app.get('/category/cat', async function (req, res) {
+    const catCateg = await prisma.category.findMany({
+        where: {
+            name: 'Kattprodukter',
+        },
+        include: {
+            items: true,
+        },
+    })
+    res.json({ data: catCateg })
+})
+
+app.get('/category/other', async function (req, res) {
+    const otherCateg = await prisma.category.findMany({
+        where: {
+            name: 'Annat',
+        },
+        include: {
+            items: true,
+        },
+    })
+    res.json({ data: otherCateg })
 })
 
 app.listen(3000)
