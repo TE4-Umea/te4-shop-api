17d6ad56c3b8a05816358b89e186e32824d20478
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-10-13 08:25:03 +0200

    Search api route

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const prisma = new PrismaClient()
 app.use(cors())
 
 app.get('/', async function (req, res) {
+    // needs pagination
+    //const { page = 1, limit = 10 } = req.query
+
     const allItems = await prisma.item.findMany({
         // orderBy: {
         //     id: 'desc',
@@ -15,12 +18,16 @@ app.get('/', async function (req, res) {
             brand: true,
             categories: true,
         },
+        //take: parseInt(limit),
+        //skip: (parseInt(page) - 1) * parseInt(limit),
     })
 
     res.json({ data: allItems })
 })
 
 app.get('/product/:id', async function (req, res) {
+    // security for id?
+
     const oneItem = await prisma.item.findMany({
         where: {
             id: parseInt(req.params.id),
@@ -32,6 +39,24 @@ app.get('/product/:id', async function (req, res) {
     })
 
     res.json({ data: oneItem })
+})
+
+app.get('/product/search/:value', async function (req, res) {
+    let value = req.params.value
+    // security?
+
+    const searchedItems = await prisma.item.findMany({
+        where: {
+            name: {
+                contains: value,
+            }
+        },
+        include: {
+            brand: true,
+            categories: true,
+        }
+    })
+    res.json({ data: searchedItems })
 })
 
 app.get('/category/', async function (req, res) {
